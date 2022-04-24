b61a667d9011823c639096cc1fe2e1209ce4ce77
Ahmed Hashem <ahmed.hashem@hey.car>
2022-04-24 06:32:24 +0200

    refactor: remove mock models and services and update api

--- src/routes/health.ts
+++ src/routes/health.ts
@@ -6,11 +6,11 @@ const baseRouter = Router();
 const { OK } = StatusCodes;
 
 // Setup routers
-baseRouter.get("/live", async (_: Request, res: Response) => {
+baseRouter.get("/live", (_: Request, res: Response) => {
   return res.status(OK).json({});
 });
 
-baseRouter.get("/ready", async (_: Request, res: Response) => {
+baseRouter.get("/ready", (_: Request, res: Response) => {
   return res.status(OK).json({});
 });
 
--- src/shared/errors.ts
+++ src/shared/errors.ts
@@ -10,25 +10,3 @@ export abstract class CustomError extends Error {
         this.HttpStatus = httpStatus;
     }
 }
-
-
-export class ParamMissingError extends CustomError {
-
-    public static readonly Msg = 'One or more of the required parameters was missing.';
-    public static readonly HttpStatus = HttpStatusCodes.BAD_REQUEST;
-
-    constructor() {
-        super(ParamMissingError.Msg, ParamMissingError.HttpStatus);
-    }
-}
-
-
-export class UserNotFoundError extends CustomError {
-
-    public static readonly Msg = 'A user with the given id does not exists in the database.';
-    public static readonly HttpStatus = HttpStatusCodes.NOT_FOUND;
-
-    constructor() {
-        super(UserNotFoundError.Msg, UserNotFoundError.HttpStatus);
-    }
-}
